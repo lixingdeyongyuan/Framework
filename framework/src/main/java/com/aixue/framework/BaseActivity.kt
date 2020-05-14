c082d5bdc0d479e67de4986669c331f8e5e8f398
lixingdeyongyuan <lixingdeyongyuan@gmail.com>
2020-05-14 12:00:52 +0800

    Activity切换动画

--- framework/src/main/java/com/aixue/framework/BaseActivity.kt
+++ framework/src/main/java/com/aixue/framework/BaseActivity.kt
@@ -1,6 +1,7 @@
 package com.aixue.framework
 
 import android.os.Bundle
+import com.aixue.framework.log.LogProxy
 import com.trello.rxlifecycle3.components.support.RxAppCompatActivity
 
 /**
@@ -13,25 +14,36 @@ open class BaseActivity : RxAppCompatActivity() {
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LogProxy.getInstance().debug(null, "${this.javaClass.name}.onCreate")
     }
 
     public override fun onStart() {
         super.onStart()
+        LogProxy.getInstance().debug(null, "${this.javaClass.name}.onStart")
     }
 
     public override fun onResume() {
         super.onResume()
+        LogProxy.getInstance().debug(null, "${this.javaClass.name}.onResume")
     }
 
     public override fun onPause() {
         super.onPause()
+        LogProxy.getInstance().debug(null, "${this.javaClass.name}.onPause")
     }
 
     public override fun onStop() {
         super.onStop()
+        LogProxy.getInstance().debug(null, "${this.javaClass.name}.onStop")
     }
 
     public override fun onDestroy() {
         super.onDestroy()
+        LogProxy.getInstance().debug(null, "${this.javaClass.name}.onDestroy")
+    }
+
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(R.anim.slide_in_from_left, R.anim.slide_out_from_right)
     }
 }
